cbb5ac35a860c3a56b167f78b4fe6fa00187a479
rprihodko <roman69.1@yandex.ru>
2023-04-20 00:27:22 +0300

    add explicit filtering

--- app/src/main/java/com/romeat/smashup/domain/mashups/GetMashupsWithSourceUseCaseImpl.kt
+++ app/src/main/java/com/romeat/smashup/domain/mashups/GetMashupsWithSourceUseCaseImpl.kt
@@ -1,16 +1,19 @@
 package com.romeat.smashup.domain.mashups
 
+import com.romeat.smashup.data.SettingsProvider
 import com.romeat.smashup.data.dto.Mashup
 import com.romeat.smashup.network.SmashupRemoteData
 import com.romeat.smashup.util.Resource
 import com.romeat.smashup.util.getResourceWithExceptionLogging
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import retrofit2.HttpException
 import javax.inject.Inject
 
 class GetMashupsWithSourceUseCaseImpl @Inject constructor(
-    private val remoteData: SmashupRemoteData
+    private val remoteData: SmashupRemoteData,
+    private val settingsProvider: SettingsProvider
 ) : GetMashupsWithSourceUseCase {
     override suspend fun invoke(sourceId: Int): Flow<Resource<List<Mashup>>> =
         getResourceWithExceptionLogging(
@@ -18,7 +21,10 @@ class GetMashupsWithSourceUseCaseImpl @Inject constructor(
             action = suspend {
                 val response = remoteData.getMashupsWithSource(sourceId)
                 if (response.isSuccessful) {
-                    response.body()!!
+                    response.body()!!.filter { mashup ->
+                        settingsProvider.explicitAllowed.value ||
+                                !mashup.explicit
+                    }.toList()
                 } else {
                     throw HttpException(response)
                 }
