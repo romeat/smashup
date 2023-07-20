5e5a80388abcc1adbb20adc64ef51fbe306ee209
rprihodko <roman69.1@yandex.ru>
2023-07-20 13:13:32 +0300

    override BottomNavigationItem to disable ripple

--- app/src/main/java/com/romeat/smashup/presentation/home/common/composables/BottomNavBar.kt
+++ app/src/main/java/com/romeat/smashup/presentation/home/common/composables/BottomNavBar.kt
@@ -1,18 +1,41 @@
 package com.romeat.smashup.presentation.home.common.composables
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.MeasureResult
+import androidx.compose.ui.layout.MeasureScope
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.romeat.smashup.navgraphs.BottomBarScreen
 import com.romeat.smashup.navgraphs.RootGraph
+import kotlin.math.max
+import kotlin.math.roundToInt
 
 @Composable
 fun BottomNavBar(
@@ -46,7 +69,7 @@ fun RowScope.AddItem(
     currentDestination: NavDestination?,
     navController: NavHostController
 ) {
-    BottomNavigationItem(
+    BottomNavigationItemNoRipple(
         label = {
             Text(text = stringResource(id = screen.titleResource))
         },
@@ -83,3 +106,198 @@ fun RowScope.AddItem(
         }
     )
 }
+
+@Composable
+fun RowScope.BottomNavigationItemNoRipple(
+    selected: Boolean,
+    onClick: () -> Unit,
+    icon: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    label: @Composable (() -> Unit)? = null,
+    alwaysShowLabel: Boolean = true,
+    selectedContentColor: Color = LocalContentColor.current,
+    unselectedContentColor: Color = selectedContentColor.copy(alpha = ContentAlpha.medium),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+    val styledLabel: @Composable (() -> Unit)? = label?.let {
+        @Composable {
+            val style = MaterialTheme.typography.caption.copy(textAlign = TextAlign.Center)
+            ProvideTextStyle(style, content = label)
+        }
+    }
+    Box(
+        modifier
+            .selectable(
+                selected = selected,
+                onClick = onClick,
+                enabled = enabled,
+                role = Role.Tab,
+                interactionSource = interactionSource,
+                indication = null
+            )
+            .weight(1f),
+        contentAlignment = Alignment.Center
+    ) {
+        BottomNavigationTransitionOverride(
+            selectedContentColor,
+            unselectedContentColor,
+            selected
+        ) { progress ->
+            val animationProgress = if (alwaysShowLabel) 1f else progress
+
+            BottomNavigationItemBaselineLayoutOverride(
+                icon = icon,
+                label = styledLabel,
+                iconPositionAnimationProgress = animationProgress
+            )
+        }
+    }
+}
+
+@Composable
+private fun BottomNavigationTransitionOverride(
+    activeColor: Color,
+    inactiveColor: Color,
+    selected: Boolean,
+    content: @Composable (animationProgress: Float) -> Unit
+) {
+    val animationProgress by animateFloatAsState(
+        targetValue = if (selected) 1f else 0f,
+    )
+
+    val color = lerp(inactiveColor, activeColor, animationProgress)
+
+    CompositionLocalProvider(
+        LocalContentColor provides color.copy(alpha = 1f),
+        LocalContentAlpha provides color.alpha,
+    ) {
+        content(animationProgress)
+    }
+}
+
+@Composable
+private fun BottomNavigationItemBaselineLayoutOverride(
+    icon: @Composable () -> Unit,
+    label: @Composable (() -> Unit)?,
+    /*@FloatRange(from = 0.0, to = 1.0)*/
+    iconPositionAnimationProgress: Float
+) {
+    Layout(
+        {
+            Box(Modifier.layoutId("icon")) { icon() }
+            if (label != null) {
+                Box(
+                    Modifier
+                        .layoutId("label")
+                        .alpha(iconPositionAnimationProgress)
+                        .padding(horizontal = BottomNavigationItemHorizontalPadding)
+                ) { label() }
+            }
+        }
+    ) { measurables, constraints ->
+        val iconPlaceable = measurables.first { it.layoutId == "icon" }.measure(constraints)
+
+        val labelPlaceable = label?.let {
+            measurables.first { it.layoutId == "label" }.measure(
+                // Measure with loose constraints for height as we don't want the label to take up more
+                // space than it needs
+                constraints.copy(minHeight = 0)
+            )
+        }
+
+        // If there is no label, just place the icon.
+        if (label == null) {
+            placeIcon(iconPlaceable, constraints)
+        } else {
+            placeLabelAndIcon(
+                labelPlaceable!!,
+                iconPlaceable,
+                constraints,
+                iconPositionAnimationProgress
+            )
+        }
+    }
+}
+
+
+/**
+ * Places the provided [iconPlaceable] in the vertical center of the provided [constraints]
+ */
+private fun MeasureScope.placeIcon(
+    iconPlaceable: Placeable,
+    constraints: Constraints
+): MeasureResult {
+    val height = constraints.maxHeight
+    val iconY = (height - iconPlaceable.height) / 2
+    return layout(iconPlaceable.width, height) {
+        iconPlaceable.placeRelative(0, iconY)
+    }
+}
+
+/**
+ * Places the provided [labelPlaceable] and [iconPlaceable] in the correct position, depending on
+ * [iconPositionAnimationProgress].
+ *
+ * When [iconPositionAnimationProgress] is 0, [iconPlaceable] will be placed in the center, as with
+ * [placeIcon], and [labelPlaceable] will not be shown.
+ *
+ * When [iconPositionAnimationProgress] is 1, [iconPlaceable] will be placed near the top of item,
+ * and [labelPlaceable] will be placed at the bottom of the item, according to the spec.
+ *
+ * When [iconPositionAnimationProgress] is animating between these values, [iconPlaceable] will be
+ * placed at an interpolated position between its centered position and final resting position.
+ *
+ * @param labelPlaceable text label placeable inside this item
+ * @param iconPlaceable icon placeable inside this item
+ * @param constraints constraints of the item
+ * @param iconPositionAnimationProgress the progress of the icon position animation, where 0
+ * represents centered icon and no label, and 1 represents top aligned icon with label.
+ * Values between 0 and 1 interpolate the icon position so we can smoothly move the icon.
+ */
+private fun MeasureScope.placeLabelAndIcon(
+    labelPlaceable: Placeable,
+    iconPlaceable: Placeable,
+    constraints: Constraints,
+    /*@FloatRange(from = 0.0, to = 1.0)*/
+    iconPositionAnimationProgress: Float
+): MeasureResult {
+    val height = constraints.maxHeight
+
+    // TODO: consider multiple lines of text here, not really supported by spec but we should
+    // have a better strategy than overlapping the icon and label
+    val baseline = labelPlaceable[LastBaseline]
+
+    val baselineOffset = 12.dp.roundToPx()
+
+    // Label should be [baselineOffset] from the bottom
+    val labelY = height - baseline - baselineOffset
+
+    val unselectedIconY = (height - iconPlaceable.height) / 2
+
+    // Icon should be [baselineOffset] from the text baseline, which is itself
+    // [baselineOffset] from the bottom
+    val selectedIconY = height - (baselineOffset * 2) - iconPlaceable.height
+
+    val containerWidth = max(labelPlaceable.width, iconPlaceable.width)
+
+    val labelX = (containerWidth - labelPlaceable.width) / 2
+    val iconX = (containerWidth - iconPlaceable.width) / 2
+
+    // How far the icon needs to move between unselected and selected states
+    val iconDistance = unselectedIconY - selectedIconY
+
+    // When selected the icon is above the unselected position, so we will animate moving
+    // downwards from the selected state, so when progress is 1, the total distance is 0, and we
+    // are at the selected state.
+    val offset = (iconDistance * (1 - iconPositionAnimationProgress)).roundToInt()
+
+    return layout(containerWidth, height) {
+        if (iconPositionAnimationProgress != 0f) {
+            labelPlaceable.placeRelative(labelX, labelY + offset)
+        }
+        iconPlaceable.placeRelative(iconX, selectedIconY + offset)
+    }
+}
+
+private val BottomNavigationItemHorizontalPadding = 12.dp
